8cf0a10365f44c7b76296af63e62ccc25c5038d1
Abijah Kajabika <Abijah.Migisho@noveogroup.com>
2022-03-29 02:12:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const drawIcon = (size) => {
 	const n = size + 1;
-	for (let i = 1; i <= n; i++){
+	for (let i = 1; i <= size; i++){
 		let  countValue = i <= n/2 ? i : n - i;
 		countValue = (countValue << (countValue > 1)) -  (countValue > 1);
 		console.log(`${' '.repeat(Math.abs(n/2 - i))}${'*'.repeat(countValue)}`);
